3ca452f30fad00353598e143aca954a147cfab76
Eduard Bardají Puig <ebardaji@gmail.com>
2022-07-29 01:33:52 +0200

    Implement search networking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import {
+  Alert,
+  Card,
+  Code,
   Container,
   Loader,
   MantineProvider,
@@ -9,12 +12,19 @@ import {
   Title,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { useState } from "react";
-import { IconSearch } from "@tabler/icons";
+import { useMemo } from "react";
+import { IconSearch, IconAlertCircle } from "@tabler/icons";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import { AccountsApi, Configuration } from "@stacks/blockchain-api-client";
 
 enum Net {
-  Testnet = "TESTNET",
-  Mainnet = "MAINNET",
+  Devnet = "http://localhost:3999",
+  Testnet = "https://stacks-node-api.testnet.stacks.co",
+  Mainnet = "https://stacks-node-api.mainnet.stacks.co",
 }
 
 const AppWithoutProviders = () => {
@@ -33,7 +43,54 @@ const AppWithoutProviders = () => {
     },
   });
 
-  const [isLoading, setIsLoading] = useState(false);
+  const config = useMemo(
+    () => new Configuration({ basePath: form.values.net }),
+    [form.values.net]
+  );
+  // const nftsApi = useMemo(
+  //   () => new NonFungibleTokensApi(config),
+  //   // () => new NonFungibleTokensApi(),
+  //   [form.values.net]
+  // );
+  const accountsApi = useMemo(() => new AccountsApi(config), [form.values.net]);
+
+  // const nftsQuery = useQuery(
+  //   ["nfts", form.values.principal],
+  //   () => nftsApi.getNftHoldings({ principal: form.values.principal }),
+  //   {
+  //     enabled: false,
+  //   }
+  // );
+  const balancesQuery = useQuery<
+    { non_fungible_tokens: { [key: string]: unknown } },
+    string
+  >(
+    ["balances", form.values.net, form.values.principal],
+    // () => {
+    //   return (async () => {
+    //     try {
+    //       return accountsApi.getAccountBalance({
+    //         principal: form.values.principal,
+    //       });
+    //     } catch (res) {
+    //       const error = (await (res as Response).json()).error;
+    //       throw error;
+    //     }
+    //   })();
+    // },
+    async () => {
+      try {
+        const res = await accountsApi.getAccountBalance({
+          principal: form.values.principal,
+        });
+        return res;
+      } catch (err) {
+        const error = (await (err as Response).json()).error;
+        throw error;
+      }
+    },
+    { enabled: false, retry: false }
+  );
 
   return (
     <>
@@ -47,12 +104,9 @@ const AppWithoutProviders = () => {
           </Text>
         </Container>
         <form
-          onSubmit={form.onSubmit(async (values) => {
-            console.log(values);
-            setIsLoading(true);
-            await new Promise((resolve) => setTimeout(resolve, 1_000));
-            setIsLoading(false);
-            console.log("results");
+          onSubmit={form.onSubmit(() => {
+            // nftsQuery.refetch();
+            balancesQuery.refetch();
           })}
         >
           <Stack>
@@ -61,6 +115,10 @@ const AppWithoutProviders = () => {
                 value={form.values.net}
                 onChange={(val: Net) => form.setFieldValue("net", val)}
                 data={[
+                  {
+                    label: "Devnet (localhost:3999)",
+                    value: Net.Devnet,
+                  },
                   {
                     label: "Testnet",
                     value: Net.Testnet,
@@ -77,22 +135,55 @@ const AppWithoutProviders = () => {
                 required
                 icon={<IconSearch size={16} />}
                 placeholder="Principal e.g. ST1B9E..."
-                rightSection={isLoading ? <Loader size="xs" /> : null}
+                rightSection={
+                  balancesQuery.isFetching ? <Loader size="xs" /> : null
+                }
                 {...form.getInputProps("principal")}
               />
             </Container>
           </Stack>
         </form>
+        {!balancesQuery.isFetching && balancesQuery.error && (
+          <Container>
+            <Alert
+              icon={<IconAlertCircle size={16} />}
+              title="Error"
+              color="red"
+            >
+              {balancesQuery.error}
+            </Alert>
+          </Container>
+        )}
+        {!balancesQuery.isFetching && balancesQuery.data && (
+          <>
+            <Container>
+              <Stack>
+                {Object.keys(balancesQuery.data.non_fungible_tokens).map(
+                  (k) => (
+                    <Card shadow="xs" key={k}>
+                      <Text weight={700}>{k.split("::")[1]}</Text>
+                      <Code>{k}</Code>
+                    </Card>
+                  )
+                )}
+              </Stack>
+            </Container>
+          </>
+        )}
       </Stack>
     </>
   );
 };
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
       <MantineProvider withGlobalStyles withNormalizeCSS>
-        <AppWithoutProviders />
+        <QueryClientProvider client={queryClient}>
+          <AppWithoutProviders />
+        </QueryClientProvider>
       </MantineProvider>
     </>
   );
